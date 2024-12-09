b102dc6a705954f0b7fa0dd7be7b500d04b426c2
Maishasan <hsnmys97@gmail.com>
2024-12-09 18:38:49 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,16 +1,14 @@
 def create_codon_dict(file_path):
+    path = "data/codons.txt"
     codon_dict = {}
     file = open(file_path)
     lines = file.readlines()
     file.close()
     for line in lines:
         line = line.strip()
-        if line:
-            parts = line.split()
-            codon = parts[0]
-            amino_acid = parts[1]
-            codon_dict[codon] = amino_acid
-        else:
-            print("invalid line")
+        parts = line.split('\t')
+        codon = parts[0]
+        amino_acid = parts[2]
+        codon_dict[codon] = amino_acid
     return codon_dict
 
